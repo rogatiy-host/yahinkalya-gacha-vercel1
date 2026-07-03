bd41eb4638a1a35449fe1dca00f0a050887df442
rogatiy-host <moiakkhsr@gmail.com>
2026-07-03 22:54:15 +0300

    ewet

--- server/middleware/admin.js
+++
@@ -1,6 +0,0 @@
-export function adminOnly(req, res, next) {
-  if (!req.user || req.user.role !== 'admin') {
-    return res.status(403).json({ error: 'Только для администратора' });
-  }
-  next();
-}
